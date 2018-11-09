bd90e18b0d04f8118d913e8928993540e438e72b
Gurparkash Singh <gsingh@student.tgm.ac.at>
2018-11-09 14:17:23 +0100

    unittests

--- src/unittest/python/GK/test_gk.py
+++ src/unittest/python/GK/test_gk.py
@@ -3,8 +3,23 @@ import unittest
 
 
 class FlaskTestCase(unittest.TestCase):
-    def test_index(self):
-        tester=app.test_client(self)
-        response=tester.get('/users',content_type='html/text')
-        self.assertEqual(response.status_code,200)
+    @classmethod
+    def setUpClass(cls):
+        pass
 
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def setUp(self):
+        # creates a test client
+        self.app = app.test_client()
+        # propagate the exceptions to the test client
+        self.app.testing = True
+
+    def test_index(self):
+        result = self.app.get('/users')
+        self.assertEqual(result.status_code,200)
+    def test_user_data(self):
+        result = self.app.get('/users/user5')
+        self.assertEqual(result.data, '{"username": "gsingh5", "email": "gsinghmail", "picture": "gsinghpic"}\n')
