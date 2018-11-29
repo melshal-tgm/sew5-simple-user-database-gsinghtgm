19aa5dae998cbad550d07e0bf9c8d0757871c1b5
Gurparkash Singh <gsingh@student.tgm.ac.at>
2018-11-29 08:59:24 +0100

    CRUD function optimized and cypress first test

--- src/main/vue/cypress/integration/testing.spec.js
+++ src/main/vue/cypress/integration/testing.spec.js
@@ -1,31 +1,17 @@
-import HelloWorld from '../../src/components/HelloWorld.vue'
-const mountVue = require('../../src')
+describe('Home', () => {
+  beforeEach(() => {
+    // Because we're only testing the homepage
+    // in this test file, we can run this command
+    // before each individual test instead of
+    // repeating it in every test.
+    cy.visit('localhost:8080/');
+  });
 
-/* eslint-env mocha */
-describe('HelloWorld.vue', () => {
-  beforeEach(mountVue(HelloWorld))
-
-  it('shows Users', () => {
-    cy.contains('Users')
-  })
-})
-
-describe('Several components', () => {
-  const template = `
-          <thead>
-            <tr>
-              <th scope="col">Username</th>
-              <th scope="col">Email</th>
-              <th scope="col">Picture</th>
-            </tr>
-          </thead>
-  `
-  const components = {
-    hello: Hello
-  }
-  beforeEach(mountVue({ template, components }))
-
-  it('greets the world 3 times', () => {
-    cy.get('p').should('have.length', 1)
-  })
-})
+  it('Should display the main headline.', () => {
+    // By using `data-qa` selectors, we can separate
+    // CSS selectors used for styling from those used
+    // exclusively for testing our application.
+    // See: https://docs.cypress.io/guides/references/best-practices.html#Selecting-Elements
+     cy.get('h1').should('contain', 'Users')
+  });
+});
