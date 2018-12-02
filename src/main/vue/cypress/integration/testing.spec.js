e30f9f6dce30f7b732c89de5e66f9faf25dd2d42
unknown <gsingh@student.tgm.ac.at>
2018-12-02 22:17:22 +0100

    Travis Cypress

--- src/main/vue/cypress/integration/testing.spec.js
+++ src/main/vue/cypress/integration/testing.spec.js
@@ -1,6 +1,5 @@
 import HelloWorld from '../../src/components/HelloWorld.vue'
 const mountVue = require('cypress-vue-unit-test')
-<<<<<<< HEAD
 describe('HelloWorld', () => {
   beforeEach(mountVue(HelloWorld))
 
@@ -8,16 +7,3 @@ describe('HelloWorld', () => {
     cy.get('h1').should('contain', 'Users')
   })
 })
-=======
-describe('HelloWorld.vue', () => {
-  beforeEach(mountVue(HelloWorld))
-
-  it('Should display the main headline.', () => {
-    // By using `data-qa` selectors, we can separate
-    // CSS selectors used for styling from those used
-    // exclusively for testing our application.
-    // See: https://docs.cypress.io/guides/references/best-practices.html#Selecting-Elements
-     cy.get('h1').should('contain', 'Users')
-  });
-});
->>>>>>> a81c92797332c06ef3738ed629d5295cb442ca3c
